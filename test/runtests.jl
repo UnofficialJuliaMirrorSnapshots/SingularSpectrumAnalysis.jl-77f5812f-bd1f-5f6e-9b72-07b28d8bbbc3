132bfae2c31127b9c391d41316ed1310a97946ea
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 02:51:31 +0000

    Snapshot of branch master taken on 2019-04-07T22:51:30.355-04:00 from "https://github.com/UnofficialJuliaMirror/SingularSpectrumAnalysis.jl-77f5812f-bd1f-5f6e-9b72-07b28d8bbbc3"

---
+++ test/runtests.jl
@@ -0,0 +1,28 @@
+using SingularSpectrumAnalysis
+using Test, Statistics
+const SSA = SingularSpectrumAnalysis
+
+# Generate some data
+L = 20
+K = 100
+N = K*L;
+t = 1:N;
+T = 20;
+y = sin.(2pi/T*t);
+y .+= (0.5sin.(2pi/T*4*t)).^2
+e = 0.1randn(N);
+ys = y+e;
+# plot(ys)
+
+USV = hsvd(ys,L)
+seasonal_groupings = [1:2, 4:5]
+trends = 3
+yrt, yrs = reconstruct(USV, trends, seasonal_groupings)
+yr = sum([yrt yrs],dims=2)
+@test sqrt(mean((y.-ys).^2)) > sqrt(mean((y.-yr).^2))
+
+
+# using Plots
+# sigmaplot(USV)
+# logsigmaplot(USV)
+# cumsigmaplot(USV)
